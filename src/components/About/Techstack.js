cae2dca7ae57866354c4e9a70101630d036fd607
hipstertester <shidqiadiatma12@gmail.com>
2023-05-28 19:12:29 +0700

    updated portfolio

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiNodejs,
+  DiJavascript1,
+  DiPython,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiCypress,
+  SiSelenium,
+  SiWebdriverio,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSelenium />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCypress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWebdriverio />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
